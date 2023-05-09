ac0e469f42e23fa0f134a4434d5ba20f6dfaa6d2
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2023-05-09 21:58:47 +0600

    register user

--- ECommerce.Web/Controllers/AccountController.cs
+++ ECommerce.Web/Controllers/AccountController.cs
@@ -9,10 +9,14 @@ namespace ECommerce.Web.Controllers;
 public class AccountController : Controller
 {
     private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public AccountController(SignInManager<IdentityUser> signInManager)
+    public AccountController(
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
     {
         _signInManager = signInManager;
+        _userManager = userManager;
     }
 
     public IActionResult SignIn(string returnUrl = null)
@@ -39,6 +43,44 @@ public class AccountController : Controller
             else
             {
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+        }
+        return View(model);
+    }
+
+    public IActionResult Register(string returnUrl = null)
+    {
+        var model = new RegisterModel();
+        model.ReturnUrl = returnUrl;
+        return View(model);
+    }
+
+    [HttpPost, ValidateAntiForgeryToken]
+    public async Task<IActionResult> RegisterAsync(RegisterModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                EmailConfirmed = true
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return LocalRedirect(model.ReturnUrl);
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
                 return View(model);
             }
         }
---
+++ ECommerce.Web/Models/Account/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Web.Models.Account;
+
+public class RegisterModel
+{
+    [Required, EmailAddress, Display(Name = "Email"), MaxLength(25, ErrorMessage = "Email can't exceed 25 characters.")]
+    public string Email { get; set; }
+
+    [Required, DataType(DataType.Password), Display(Name = "Password")]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password), Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+    public string ReturnUrl { get; set; }
+}
