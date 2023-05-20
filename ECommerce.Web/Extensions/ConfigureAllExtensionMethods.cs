a357c6d7c7ff141778a75259f41ca06d1104a7e9
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2023-05-20 12:33:51 +0600

    program.cs minimized

---
+++ ECommerce.Web/Extensions/ConfigureAllExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿namespace ECommerce.Web.Extensions;
+
+internal static class ConfigureAllExtensionMethods
+{
+    internal static void ConfigureAllExtensions(this WebApplicationBuilder builder)
+    {
+        builder.Services.ConfigureDbContext(builder); // AppDbContext configuration
+        builder.Services.ConfigureIdentity(); // Identity configuration
+        builder.Services.ConfigureCommonClasses(builder); // Common classes configuration
+        builder.Host.ConfigureAutofac(); // Using Autofac as dependency container
+        builder.Host.ConfigureLogger(); // Logger
+        builder.Services.ConfigureCookie(); // Cookie configuration
+        builder.Services.ConfigureSession(); // Session Configuration
+
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddRazorPages();
+    }
+}
--- ECommerce.Web/Program.cs
+++ ECommerce.Web/Program.cs
@@ -1,49 +1,21 @@
-using Autofac.Extensions.DependencyInjection;
 using ECommerce.Web.Extensions;
-using ECommerce.Web.Helpers;
 using ECommerce.Web.Middlewares;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    // Load settings via extensions
-    builder.Services.ConfigureDbContext(builder); // AppDbContext configuration
-    builder.Services.ConfigureIdentity(); // Identity configuration
-    builder.Services.ConfigureCommonClasses(builder); // Common classes configuration
-    builder.Host.ConfigureAutofac(); // Using Autofac as dependency container
-    builder.Host.ConfigureLogger(); // Logger
-    builder.Services.ConfigureCookie(); // Cookie configuration
-    builder.Services.ConfigureSession(); // Session Configuration
-
-    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-    builder.Services.AddControllersWithViews();
-    builder.Services.AddHttpContextAccessor();
-    builder.Services.AddRazorPages();
+    // Load via service extensions
+    builder.ConfigureAllExtensions();
 }
 
 try
 {
     // Register Request pipelines
     var app = builder.Build();
-    app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
-    app.Services.GetAutofacRoot();
     Log.Information("----------Application booting.----------");
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseMigrationsEndPoint();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Home/Error"); // /Home/Error
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    AdminDataSeeder.LoadAdminDataAndRole(app.Services).Wait();
-
-    app.ConfigureMiddlewares(builder);
+    // Load via middleware extensions
+    app.ConfigureAllMiddlewares(builder);
 
     app.Run();
 }
